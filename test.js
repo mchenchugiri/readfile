c93333cd9dbf024aec15a476eec5d13ad9e67dd4
mchenchugiri <76865322+mchenchugiri@users.noreply.github.com>
2024-04-03 16:24:14 +0530

    Add files via upload

---
+++ test.js
@@ -0,0 +1,8 @@
+UI.getEntity().then(function(entity){
+UI.prompt(text,
+defaultText).then(function(response){
+@param text {String} prompt text
+           @param defaultText {String?} prompt default value  
+});
+   
+});
